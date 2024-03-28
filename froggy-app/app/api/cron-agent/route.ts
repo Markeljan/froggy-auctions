2df92282faff620d7afcf10991dc5f59c0be8123
Markeljan Sokoli <Markeljan19@gmail.com>
2024-03-28 12:12:36 -0400

    refactor cron

--- froggy-app/app/api/cron-agent/route.ts
+++ froggy-app/app/api/cron-agent/route.ts
@@ -39,6 +39,7 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
   if (dbPendingTxs.length === 0) {
     return NextResponse.json({ message: "No pending hops to execute" }, { status: 200 });
   }
+
   const nonce = await getNonce(FROGGY_AGENT_ADDRESS, network);
   const executedTxIds: string[] = [];
   const dbTxById = new Map(dbPendingTxs.map((hop) => [hop.txId, hop]));
@@ -50,152 +51,151 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
     })
   )) as FroggyHopContractCall[];
 
-  // filter out the successful transactions
-  const successfulTransactions = liveTransactions.filter((tx) => {
-    if (!tx) {
-      return false;
-    }
-    return tx.tx_status === "success";
-  });
-  // handle HOPPING transactions
+  // filter successful transactions
+  const successfulTransactions = liveTransactions.filter((tx) => tx.tx_status === "success");
+
   successfulTransactions.forEach(async (tx) => {
-    const matchingHop = dbTxById.get(tx.tx_id);
-    if (matchingHop?.hopStatus !== HopStatus.HOPPING) {
-      return null;
+    const matchingTx = dbTxById.get(tx.tx_id);
+    if (!matchingTx) {
+      console.error(`No matching tx found for txId: ${tx.tx_id}`);
+      return;
     }
-    const contractAddress = tx?.contract_call?.contract_id?.split(".")[0];
-    const contractFunctionName = tx?.contract_call?.function_name;
-    if (contractAddress !== SORDINALS_CONTRACT_ADDRESS || contractFunctionName !== "transfer-memo-single") {
-      return null;
-    }
-    const sender = tx.sender_address;
-    const recipient = tx?.contract_call?.function_args[0]?.repr.split("'")[1];
-    if (recipient !== FROGGY_AGENT_ADDRESS || !sender) {
-      return null;
-    }
-    const memo = tx?.contract_call?.function_args[1]?.repr;
-    if (!memo?.startsWith(TRANSFER_PREFIX)) {
-      return null;
-    }
-    const inscriptionHashFromMemo = memo.slice(TRANSFER_PREFIX.length);
-    const { inscriptionId, id: tokenId } = findFroggy(inscriptionHashFromMemo) || {};
-    if (!inscriptionId || !tokenId) {
-      return null;
-    }
-    const agentFroggys = await getOwnedFroggysByPrinciple(FROGGY_AGENT_ADDRESS);
-    const isFroggyHeldByAgent = agentFroggys?.some((sord: { id: string }) => sord.id === inscriptionId.toString());
-    if (!isFroggyHeldByAgent) {
-      return null;
+    // handle HOPPING transactions
+    if (matchingTx.hopStatus === HopStatus.HOPPING) {
+      const contractAddress = tx?.contract_call?.contract_id?.split(".")[0];
+      const contractFunctionName = tx?.contract_call?.function_name;
+      if (contractAddress !== SORDINALS_CONTRACT_ADDRESS || contractFunctionName !== "transfer-memo-single") {
+        console.error("Invalid contract address or function name");
+        return;
+      }
+      const sender = tx.sender_address;
+      const recipient = tx?.contract_call?.function_args[0]?.repr.split("'")[1];
+      if (recipient !== FROGGY_AGENT_ADDRESS || !sender) {
+        console.error("Invalid sender or recipient");
+        return;
+      }
+      const memo = tx?.contract_call?.function_args[1]?.repr;
+      if (!memo?.startsWith(TRANSFER_PREFIX)) {
+        console.error("Invalid memo");
+        return;
+      }
+      const inscriptionHashFromMemo = memo.slice(TRANSFER_PREFIX.length);
+      const { inscriptionId, id: tokenId } = findFroggy(inscriptionHashFromMemo) || {};
+      if (!inscriptionId || !tokenId) {
+        console.error("Invalid inscriptionId or tokenId");
+        return;
+      }
+      const agentFroggys = await getOwnedFroggysByPrinciple(FROGGY_AGENT_ADDRESS);
+      const isFroggyHeldByAgent = agentFroggys?.some((sord: { id: string }) => sord.id === inscriptionId.toString());
+      if (!isFroggyHeldByAgent) {
+        console.error(`Froggy inscription not held by agent: ${inscriptionId}`);
+        return;
+      }
+
+      const tokenIdHolder = await getFroggyTokenOwner(tokenId);
+      const isTokenIdVaulted = tokenIdHolder === `${FROGGY_CONTRACT_ADDRESS}.froggys`;
+
+      const contractSendsNFTPostCondition = createNonFungiblePostCondition(
+        FROGGY_CONTRACT_ADDRESS,
+        NonFungibleConditionCode.Sends,
+        createAssetInfo(FROGGY_CONTRACT_ADDRESS, "froggys", "froggys"),
+        uintCV(tokenId)
+      );
+
+      const txOptions: SignedContractCallOptions = {
+        anchorMode: AnchorMode.Any,
+        contractAddress: FROGGY_CONTRACT_ADDRESS,
+        contractName: "froggys",
+        functionName: "hop",
+        functionArgs: [uintCV(tokenId), principalCV(sender)],
+        senderKey: FROGGY_AGENT_KEY,
+        network: network,
+        postConditions: isTokenIdVaulted ? [contractSendsNFTPostCondition] : [],
+        fee: 100000n, // 0.10 STX
+        nonce: nonce + BigInt(executedTxIds.length),
+      };
+      // execute the hop
+      const hopTxId = await executeFroggyTransaction(txOptions, matchingTx, HopStatus.HOPPED);
+      if (hopTxId) {
+        executedTxIds.push(hopTxId);
+      }
     }
 
-    const tokenIdHolder = await getFroggyTokenOwner(tokenId);
-    const isTokenIdVaulted = tokenIdHolder === `${FROGGY_CONTRACT_ADDRESS}.froggys`;
+    // handle HOPPING-BACK transactions
+    if (matchingTx.hopStatus === HopStatus.HOPPING_BACK) {
+      const contractAddress = tx?.contract_call?.contract_id?.split(".")[0];
+      const contractFunctionName = tx?.contract_call?.function_name;
+      if (contractAddress !== FROGGY_CONTRACT_ADDRESS || contractFunctionName !== "hop-back") {
+        console.error("Invalid contract address or function name");
+        return false;
+      }
+      // get the tokenId from the tx function args
+      const tokenId = parseInt(tx?.contract_call?.function_args[0]?.repr.split("u")[1]);
+      const { inscriptionId, inscriptionHash } = findFroggy(tokenId) || {};
+      if (!inscriptionHash || !inscriptionId) {
+        console.error(`Invalid inscriptionId or inscriptionHash: ${inscriptionId} ${inscriptionHash}`);
+        return false;
+      }
+      // validate that the sender owns the hopped froggy
+      const tokenIdHolder = await getFroggyTokenOwner(tokenId);
 
-    const contractSendsNFTPostCondition = createNonFungiblePostCondition(
-      FROGGY_CONTRACT_ADDRESS,
-      NonFungibleConditionCode.Sends,
-      createAssetInfo(FROGGY_CONTRACT_ADDRESS, "froggys", "froggys"),
-      uintCV(tokenId)
-    );
+      const sender = tx.sender_address;
+      if (tokenIdHolder !== sender) {
+        console.error(`Token holder does not match sender: ${tokenIdHolder} !== ${sender}`);
+        return false;
+      }
 
-    const txOptions: SignedContractCallOptions = {
-      anchorMode: AnchorMode.Any,
-      contractAddress: FROGGY_CONTRACT_ADDRESS,
-      contractName: "froggys",
-      functionName: "hop",
-      functionArgs: [uintCV(tokenId), principalCV(sender)],
-      senderKey: FROGGY_AGENT_KEY,
-      network: network,
-      postConditions: isTokenIdVaulted ? [contractSendsNFTPostCondition] : [],
-      fee: 100000n, // 0.10 STX
-      nonce: nonce + BigInt(executedTxIds.length),
-    };
-    // execute the hop
-    const hopTxId = await executeFroggyTransaction(txOptions, matchingHop, HopStatus.HOPPED);
-    if (hopTxId) {
-      executedTxIds.push(hopTxId);
+      // validate that the agent owns the froggy inscription
+      const agentFroggys = await getOwnedFroggysByPrinciple(FROGGY_AGENT_ADDRESS);
+      const isFroggyHeldByAgent = agentFroggys?.some((sord: { id: string }) => sord.id === inscriptionId.toString());
+      if (!isFroggyHeldByAgent) {
+        console.error(`Froggy inscription not held by agent: ${inscriptionId}`);
+        return false;
+      }
+      const memoString = Buffer.from("t").toString("hex") + inscriptionHash;
+      const txOptions: SignedContractCallOptions = {
+        anchorMode: AnchorMode.Any,
+        contractAddress: SORDINALS_CONTRACT_ADDRESS,
+        contractName: "sordinals-inscribe",
+        functionName: "transfer-memo-single",
+        functionArgs: [principalCV(sender), bufferCV(Buffer.from(memoString, "hex"))],
+        senderKey: FROGGY_AGENT_KEY,
+        network: network,
+        fee: 100000n, // 0.10 STX
+        nonce: nonce + BigInt(executedTxIds.length),
+        postConditions: [createSTXPostCondition(FROGGY_AGENT_ADDRESS, FungibleConditionCode.Equal, 1n)],
+      };
+      // execute the hop-back
+      const hopBackTxId = await executeFroggyTransaction(txOptions, matchingTx, HopStatus.NONE);
+      if (hopBackTxId) {
+        executedTxIds.push(hopBackTxId);
+      }
+    }
+
+    if (matchingTx.hopStatus === HopStatus.HOPPED || matchingTx.hopStatus === HopStatus.NONE) {
+      await updateFroggyHop({ ...matchingTx, txStatus: TxStatus.SUCCESS });
     }
   });
 
-  // handle HOPPING-BACK transactions
-  successfulTransactions.forEach(async (tx) => {
-    const matchingHop = dbTxById.get(tx.tx_id);
-    if (matchingHop?.hopStatus !== HopStatus.HOPPING_BACK) {
-      return null;
-    }
-    const contractAddress = tx?.contract_call?.contract_id?.split(".")[0];
-    const contractFunctionName = tx?.contract_call?.function_name;
-    if (contractAddress !== FROGGY_CONTRACT_ADDRESS || contractFunctionName !== "hop-back") {
-      return false;
-    }
-    // get the tokenId from the tx function args
-    const tokenId = parseInt(tx?.contract_call?.function_args[0]?.repr.split("u")[1]);
-    const { inscriptionId, inscriptionHash } = findFroggy(tokenId) || {};
-    if (!inscriptionHash || !inscriptionId) {
-      return false;
-    }
-    // validate that the sender owns the hopped froggy
-    const tokenIdHolder = await getFroggyTokenOwner(tokenId);
+  if (executedTxIds.length === 0) {
+    return NextResponse.json({ message: "No hops executed" }, { status: 200 });
+  }
 
-    const sender = tx.sender_address;
-    if (tokenIdHolder !== sender) {
-      console.error(`Token holder does not match sender: ${tokenIdHolder} !== ${sender}`);
-      return false;
-    }
-
-    // validate that the agent owns the froggy inscription
-    const agentFroggys = await getOwnedFroggysByPrinciple(FROGGY_AGENT_ADDRESS);
-    const isFroggyHeldByAgent = agentFroggys?.some((sord: { id: string }) => sord.id === inscriptionId.toString());
-    if (!isFroggyHeldByAgent) {
-      console.error(`Froggy inscription not held by agent: ${inscriptionId}`);
-      return false;
-    }
-    const memoString = Buffer.from("t").toString("hex") + inscriptionHash;
-    const txOptions: SignedContractCallOptions = {
-      anchorMode: AnchorMode.Any,
-      contractAddress: SORDINALS_CONTRACT_ADDRESS,
-      contractName: "sordinals-inscribe",
-      functionName: "transfer-memo-single",
-      functionArgs: [principalCV(sender), bufferCV(Buffer.from(memoString, "hex"))],
-      senderKey: FROGGY_AGENT_KEY,
-      network: network,
-      fee: 100000n, // 0.10 STX
-      nonce: nonce + BigInt(executedTxIds.length),
-      postConditions: [createSTXPostCondition(FROGGY_AGENT_ADDRESS, FungibleConditionCode.Equal, 1n)],
-    };
-    // execute the hop-back
-    const hopBackTxId = await executeFroggyTransaction(txOptions, matchingHop, HopStatus.NONE);
-    if (hopBackTxId) {
-      executedTxIds.push(hopBackTxId);
-    }
-  });
-
-  // handle HOPPED and NONE pending transactions (update the txStatus on dbFroggyHop)
-  successfulTransactions.forEach(async (tx) => {
-    const matchingHop = dbTxById.get(tx.tx_id);
-    if (!matchingHop) {
-      return null;
-    }
-    if (matchingHop.hopStatus === HopStatus.HOPPED || matchingHop.hopStatus === HopStatus.NONE) {
-      await updateFroggyHop({ ...matchingHop, txStatus: TxStatus.SUCCESS });
-    }
-  });
-
-  // return executed txIds
-  return NextResponse.json({ executedTxIds }, { status: 200 });
+  return NextResponse.json({ message: "Hops executed", executedTxIds }, { status: 200 });
 }
 
 async function executeFroggyTransaction(
   txOptions: SignedContractCallOptions,
   dbFroggyHop: FroggyHop,
   newHopStatus: HopStatus
-): Promise<string | null> {
+): Promise<string | undefined> {
   const hopTransaction = await makeContractCall(txOptions);
   const broadcastResponse = await broadcastTransaction(hopTransaction, network);
   const { txid, error, reason, reason_data } = broadcastResponse;
-  if (error) {
+  console.log("broadcastResponse", broadcastResponse);
+  if (error || !txid) {
     console.error("Failed to hop", error, reason, reason_data);
-    return null;
+    return;
   }
   // update the previous dbFroggyHop
   await updateFroggyHop({ ...dbFroggyHop, txStatus: TxStatus.SUCCESS });
